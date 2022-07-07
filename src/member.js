de24cc65e0240110300d08e18e0610f868712de2
yuumusJS <yuumuknn@gmail.com>
2022-07-07 20:15:34 +0900

    getMembers リファクタ

--- src/member.js
+++ src/member.js
@@ -2,16 +2,10 @@ export const getMembers = parsedTalk => {
   // トーク内すべてのメンバー名が重複を排除せず格納される
   const members = [];
   parsedTalk.forEach(talk => {
+    
     // membersにtalksOfDay単位のメンバー名を追記する
-    Array.prototype.push.apply(
-      members,
-      talk.talksOfDay.map(t => {
-        return t.user;
-      })
-    );
+    members.push(...talk.talksOfDay.map(t => t.user));
   });
 
-  return members.filter((value, index, self) => {
-    return self.indexOf(value) === index;
-  });
+  return [...new Set(members)];
 };
